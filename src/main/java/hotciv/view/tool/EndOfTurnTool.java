b18fc5bd50430d4c3d1d47cea148051c771fe640
Sebastian Nielsen <sebastiangrundet@hotmail.dk>
2020-11-09 16:30:51 +0100

    Implemented 36.42 EndOfTurn tool

--- src/main/java/hotciv/view/tool/EndOfTurnTool.java
+++ src/main/java/hotciv/view/tool/EndOfTurnTool.java
@@ -2,28 +2,45 @@ package hotciv.view.tool;
 
 import hotciv.framework.Game;
 import minidraw.framework.DrawingEditor;
+import minidraw.framework.Figure;
 import minidraw.standard.NullTool;
 
+import java.awt.*;
 import java.awt.event.MouseEvent;
+
+import static hotciv.view.GfxConstants.TURN_SHIELD_X;
+import static hotciv.view.GfxConstants.TURN_SHIELD_Y;
 
 /** Template for the EndOfTurn Tool exercise (FRS 36.42)...
  *
- * @author Henrik Bærbak Christensen, Aarhus University
+ * A specialized tool to end the turn (the endOfTurn) only
+ * when the top shield in the age section is clicked.
  */
 public class EndOfTurnTool extends NullTool {
-  private final DrawingEditor editor;
-  private final Game game;
+	private final DrawingEditor editor;
+	private final Game game;
 
-  public EndOfTurnTool(DrawingEditor editor, Game game) {
-    this.editor = editor;
-    this.game = game;
-  }
+	public EndOfTurnTool(DrawingEditor editor, Game game) {
+		this.editor = editor;
+		this.game = game;
+	}
 
-  @Override
-  public void mouseDown(MouseEvent e, int x, int y) {
-    super.mouseDown(e, x, y);
-    // TODO: Remove print statement, and implement end-of-turn behaviour
-    System.out.println("TODO: EndOfTurn tool received 'mouse down' event");
-  }
+	@Override
+	public void mouseDown(MouseEvent e, int x, int y) {
+		super.mouseDown(e, x, y);
+		// TODONE: Remove print statement, and implement end-of-turn behaviour
+
+		Figure figure = editor.drawing().findFigure(x, y);
+
+		if (figure == null) return;
+
+		Point point = figure.displayBox().getLocation();
+
+		if (point.getX() == TURN_SHIELD_X &&
+				point.getY() == TURN_SHIELD_Y){
+			game.endOfTurn();
+		}
+
+	}
 
 }
